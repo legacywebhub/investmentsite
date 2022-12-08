b2517d27a773d4988f6fb09b1609ea91cd4c56ff
legacywebhub <legacywebhub@gmail.com>
2022-12-08 04:58:33 +0100

    Update settings.py

--- miningsite/settings.py
+++ miningsite/settings.py
@@ -142,8 +142,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR / 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # Media configuration
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_URL = '/bitinvest/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'bitinvest')
 # Cloudinary media storage
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
